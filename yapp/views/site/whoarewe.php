43783ecc4445e7acd19f3c6f7c1255931fa9200b
Quadrosh <quadrosh@gmail.com>
2018-12-05 22:00:25 +0300

    nophone

--- yapp/views/site/whoarewe.php
+++ yapp/views/site/whoarewe.php
@@ -3,8 +3,8 @@ $quotes = \app\models\Quote::find()->all();
 ?>
 
 <div id="centerlead" class="centerlead">
-    <h1>+7(960) 506-78-94</h1>
-    <h4>Иваново</h4>
+<!--    <h1>+7(960) 506-78-94</h1>-->
+<!--    <h4>Иваново</h4>-->
 </div>
 
 
